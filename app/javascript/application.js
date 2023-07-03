2e2ec6b95b17811607fdbe950e5b5d69a38b6f6f
Olexandr Matkovsky <savio.km.ua@gmail.com>
2023-07-03 23:26:55 +0300

    Added js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,6 @@
 // Entry point for the build script in your package.json
+import './add_jquery'
+import 'admin-lte'
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
